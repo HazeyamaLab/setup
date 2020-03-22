a46bf1dd631d95a4a05ece3521ee856cf1266ae2
山田侑樹 <e155417k@st.u-gakugei.ac.jp>
2020-03-22 13:10:36 +0900

    refactory: せっかくなので少しお洒落にする

--- macOS/install.sh
+++ macOS/install.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-echo "installing homebrew..."
+log='
+   _   _                                             _          _     
+  | | | | __ _ _______ _   _  __ _ _ __ ___   __ _  | |    __ _| |__  
+  | |_| |/ _` |_  / _ \ | | |/ _` | `_ ` _ \ / _` | | |   / _` | `_ \ 
+  |  _  | (_| |/ /  __/ |_| | (_| | | | | | | (_| | | |__| (_| | |_) |
+  |_| |_|\__,_/___\___|\__, |\__,_|_| |_| |_|\__,_| |_____\__,_|_.__/ 
+                      |___/                                          
+
+  See the README for documentation.
+    https://github.com/HazeyamaLab/setup
+
+  Copyright (c) 2020 "Tokyo Gakugei University - Hazeyamalab"
+'
+
+echo "$log"
+
+echo "[1] installing xcode-select..."
+which xcode-select >/dev/null 2>&1 || xcode-select --install
+
+echo "[2] installing homebrew..."
 which brew >/dev/null 2>&1 || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-echo "installing package..."
+echo "[3] download Brewfile..."
+cd "$HOME" && curl -fsSL https://raw.githubusercontent.com/HazeyamaLab/setup/master/macOS/Brewfile > ./Brewfile
+
+echo "[4] installing package..."
 brew bundle
